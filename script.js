da74fc40261e6e684e2e65e8e601346814f54e8d
Flora Roulle <flora.borson@cegetel.net>
2026-06-01 15:33:36 +0200

    Ajout bouton Pause et Reprendre

--- script.js
+++ script.js
@@ -54,16 +54,28 @@ function afficherInventaire(inventaire){
 // Text to speech
 let button = document.getElementById("analyse_btn");
 let buttonStop = document.getElementById("stop_tts")
+let buttonPause = document.getElementById("pause_tts")
+let buttonResume = document.getElementById("reprendre_tts")
 let content = document.getElementById("listInventaire");
 
 button.addEventListener("click", function(){
   let text = content.textContent;
 
-  let speech = new SpeechSynthesisUtterance(`J'ai détecté : ${text}`);
+  const speech = new SpeechSynthesisUtterance(`J'ai détecté : ${text}`);
+  speech.lang = "fr-FR";
+  speech.rate = 1;
+  speech.pitch = 1;
   speechSynthesis.speak(speech)
-
 })
 
 buttonStop.addEventListener("click", function(){
   speechSynthesis.cancel()
+})
+
+buttonPause.addEventListener("click", function(){
+  speechSynthesis.pause()
+})
+
+buttonResume.addEventListener("click", function(){
+  speechSynthesis.resume()
 })
