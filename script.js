600c6249e595d93c4d37d7b72076559455f2f210
Mario <mariomorkos98@gmail.com>
2026-06-02 09:43:21 +0200

    ajout de fichier historique

--- script.js
+++ script.js
@@ -2,64 +2,49 @@
 const analyser = document.getElementById("analyse_btn");
 analyser.addEventListener("click", GoCoco);
 
-//Declaration canvas
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-  
-
 // On charge l'image et on l'affiche avec possibilité de la remplacer
 const image = document.getElementById("image");
 
-upload.addEventListener("change", function () {
+    upload.addEventListener("change", function () {
 
-  const file = this.files[0];
+      const file = this.files[0];
 
-  // Taille canvas = taille image
-  canvas.width = image.width;
-  canvas.height = image.height;
+      if (file) {
 
-      
-  // nettoyage canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // Création d'une URL temporaire
+        const imageUrl = URL.createObjectURL(file);
 
-  if (file) {
-
-    // Création d'une URL temporaire
-    const imageUrl = URL.createObjectURL(file);
-
-    // Fonction exécutée seulement quand l'image est chargée
-    image.onload = () => {
-      console.log("Image chargée !");
+        // Fonction exécutée seulement quand l'image est chargée
+        image.onload = () => {
+        console.log("Image chargée !");
        
-    };
+        };
 
-    image.src = imageUrl;
+        image.src = imageUrl;
         image.style.display = "block";
       }
     });
     
 async function GoCoco(){
-  // Load the model.
+ // Load the model.
   let model = await cocoSsd.load();
   
-  // Predictions et Detection
+// Predictions et Detection
   const predictions = await model.detect(image);
   console.log("Predictions:", predictions);
- 
-  genererInventaire(predictions)
-  dessinerBoundingBoxes(predictions);
+  //let result = predictions
+ genererInventaire(predictions)
 }
 
 async function genererInventaire(result){
     let inventaire = {};
     result.forEach(objet => {
-    if (inventaire[objet.class] === undefined){
-      inventaire[objet.class] = 1;
-    }else {
-      inventaire[objet.class] +=1
-    }
-    console.log(inventaire)
+      if (inventaire[objet.class] === undefined){
+        inventaire[objet.class] = 1;
+      }else {
+        inventaire[objet.class] +=1
+      }
+      console.log(inventaire)
     })
     afficherInventaire(inventaire)
     genererPhrase(inventaire)
@@ -75,10 +60,10 @@ async function afficherInventaire(inventaire){
   for (i in inventaire){
     console.log(i)
     for (let j = 0; j < json["Produits"].length; j++){
-      if (i == json["Produits"][j]["nom"]){
-        const li = document.createElement("li");
-        li.textContent = `${inventaire[i]} ${json["Produits"][j]["TraductionFR"]} -- ${json["Produits"][j]["definition"]}`;
-        ul.appendChild(li);
+    if (i == json["Produits"][j]["nom"]){
+      const li = document.createElement("li");
+      li.textContent = `${inventaire[i]} ${json["Produits"][j]["TraductionFR"]} -- ${json["Produits"][j]["definition"]}`;
+      ul.appendChild(li);
     }
   }
   }
@@ -118,41 +103,3 @@ function lirePhrase(content){
   buttonResume.addEventListener("click", function(){
     speechSynthesis.resume()
   })
-
-
-function dessinerBoundingBoxes(predictions) {
-  // Taille canvas = taille image
-  canvas.width = image.width;
-  canvas.height = image.height;
-
-  // nettoyage canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  predictions.forEach(prediction => {
-
-    const [x, y, width, height] = prediction.bbox;
-
-    // Rectangle
-    ctx.strokeStyle = "green";
-    ctx.lineWidth = 5;
-    ctx.strokeRect(x, y, width, height);
-
-    // Texte
-    const texte = prediction.class + " (" + Math.round(prediction.score * 100) + "%)";
-    ctx.font = "16px Arial";
-
-    const textWidth = ctx.measureText(texte).width;
-
-    // Fond du texte
-    ctx.fillStyle = "red";
-    ctx.fillRect(
-      x, y > 25 ? y - 25 : y, textWidth + 10, 25
-    );
-
-    // Texte blanc
-    ctx.fillStyle = "white";
-    ctx.fillText(
-      texte, x + 5, y > 25 ? y - 8 : y + 17
-    );
-  });
-}
